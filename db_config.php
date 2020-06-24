7635e12a8ffa5653459354d7fb2b27b61109413c
Kool35T <Kool35T@users.noreply.github.com>
2020-06-24 11:45:40 +0800

    Create db_config.php

---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php //define database connection variables as constant
+define('DB_USER', "root"); // database user
+define('DB_PASSWORD', ""); // database password
+define('DB_DATABASE', "pessdb"); // database name
+define('DB_SERVER', "localhost"); // db server
+?>
